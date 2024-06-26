2489f4390cbd74772a8ad97e078bd7174ae8f413
Sarmat <skhachirov12@gmail.com>
2024-06-26 07:52:17 +0300

    remove padding

--- program/src/state.rs
+++ program/src/state.rs
@@ -687,8 +687,7 @@ pub struct AmmInfo {
     pub lp_amount: u64,
     /// client order id
     pub client_order_id: u64,
-    /// padding
-    pub padding2: [u64; 2],
+    // pub padding2: [u64; 2],
 }
 impl_loadable!(AmmInfo);
 
@@ -820,8 +819,8 @@ impl AmmInfo {
         self.min_price_multiplier = 1;
         self.max_price_multiplier = 1000000000;
         self.client_order_id = 0;
-        self.padding1 = Zeroable::zeroed();
-        self.padding2 = Zeroable::zeroed();
+        // self.padding1 = Zeroable::zeroed();
+        // self.padding2 = Zeroable::zeroed();
 
         Ok(())
     }
